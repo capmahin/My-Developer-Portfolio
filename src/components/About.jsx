433c8e29a02460b55ee030a2966c64e985905661
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-03-20 04:30:26 +0600

    add writing

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div><div>
+        <div>
+            <p>About</p>
+        </div>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis, cupiditate assumenda error tenetur hic illo, deserunt culpa mollitia sequi minus cum aspernatur nemo inventore voluptates ipsum pariatur maiores quis nobis placeat soluta praesentium deleniti temporibus. Veniam temporibus dolorem nobis neque illum labore ex eius earum, ipsam ipsa, iusto pariatur dolores.
+        </p>
+        <br />
+        <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque rerum enim harum consectetur repellendus fuga soluta suscipit eligendi minus commodi explicabo asperiores fugiat qui unde inventore eveniet veritatis maxime officia nam, aliquam sint temporibus. Quia veniam iusto sapiente ullam repellat, at architecto provident ducimus corrupti. Excepturi porro vero atque dolorum!
+        </p>
+        </div></div>
   )
 }
 
