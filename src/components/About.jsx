0e192178b434cd6ebfc9c626164590117afcd3fa
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-03-20 04:38:00 +0600

    add another some class

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,8 +4,8 @@ const About = () => {
   return (
     <div name="about" className="w-full h-screen bg-gradient-to-b from-purple-900 to-black text-white">
         <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
-        <div>
-            <p>About</p>
+        <div className="pb-8">
+            <p className="text-4xl font-bold inline border-b-4 border-purple-900">About</p>
         </div>
         <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis, cupiditate assumenda error tenetur hic illo, deserunt culpa mollitia sequi minus cum aspernatur nemo inventore voluptates ipsum pariatur maiores quis nobis placeat soluta praesentium deleniti temporibus. Veniam temporibus dolorem nobis neque illum labore ex eius earum, ipsam ipsa, iusto pariatur dolores.
